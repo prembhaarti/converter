e1160a5abaeb1f6b8fb04a39fb3fa1e6afbbfed1
Prem Bharti <prem.b@flipkart.com>
2020-01-27 12:36:25 +0530

    adding more string function

--- src/main/java/com/love/converter/string/StringOpsHandler.java
+++ src/main/java/com/love/converter/string/StringOpsHandler.java
@@ -1,10 +1,19 @@
 package com.love.converter.string;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class StringOpsHandler {
+    public static void main(String[] args) {
+        String data = "attrib:xyz:href:b";
+        System.out.println(StringUtils.substringAfter(data, ":"));
+                
+    }
 
     public static List<String> getTrimmedList(List<String> stringList){
         return stringList.stream().map(
@@ -16,4 +25,22 @@ public class StringOpsHandler {
         List<String> splittedData = Arrays.asList(data.split(seperator));
         return getTrimmedList(splittedData);
     }
+
+
+    public static String getJoinedString(List<String> list, String joinerString){
+        StringJoiner joiner = new StringJoiner(joinerString);
+        for(String data: list){
+            joiner.add(data);
+        }
+        return joiner.toString();
+    }
+
+    public static String getJoinedStringFromSet(Set<String> list, String joinerString){
+        StringJoiner joiner = new StringJoiner(joinerString);
+        for(String data: list){
+            joiner.add(data);
+        }
+        return joiner.toString();
+    }
+
 }
